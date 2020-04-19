18ed2b3ca8240b4b2ace23f7bba9c391efa667c3
Morormo <morormo@gmail.com>
2020-04-19 13:53:38 +0200

    Problem with store

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Login from './components/views/Login/Login';
 import Tables from './components/views/Tables/Tables';
 import TablesBookingNew from './components/views/TablesBookingNew/TablesBookingNew';
 import TablesEventsNew from './components/views/TablesEventsNew/TablesEventsNew';
-import Waiter from './components/views/Waiter/Waiter';
 import { StylesProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
@@ -17,39 +16,43 @@ import WaiterOrderNew from './components/views/WaiterOrderNew/WaiterOrderNew';
 import TablesBookingId from './components/views/TablesBookingId/TablesBookingId';
 import TablesEventsId from './components/views/TablesEventsId/TablesEventsId';
 
+import { Provider } from 'react-redux';
+import WaiterContainer from './components/views/Waiter/WaiterContainer';
+
 const theme = createMuiTheme({
   palette: {
     primary: {main: '#2B4C6F'},
-    // secondary: {main: '#11cb5f',},
   },
 });
 
 function App() {
   return (
-    <div>
-      <BrowserRouter basename={'/panel'}>
-        <StylesProvider injectFirst>
-          <ThemeProvider theme={theme}>
-            <MainLayout>
-              <Switch>
-                <Route exact path={process.env.PUBLIC_URL + '/'} component={Homepage} />
-                <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-                <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-                <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-                <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={TablesBookingNew} />
-                <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={TablesBookingId} />
-                <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={TablesEventsNew} />
-                <Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={TablesEventsId} />
-                <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-                <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={WaiterOrderNew} />
-                <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={WaiterOrderId} />
+    <Provider store={store}>
+      <div>
+        <BrowserRouter basename={'/panel'}>
+          <StylesProvider injectFirst>
+            <ThemeProvider theme={theme}>
+              <MainLayout>
+                <Switch>
+                  <Route exact path={process.env.PUBLIC_URL + '/'} component={Homepage} />
+                  <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+                  <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+                  <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+                  <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={TablesBookingNew} />
+                  <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={TablesBookingId} />
+                  <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={TablesEventsNew} />
+                  <Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={TablesEventsId} />
+                  <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={WaiterContainer} />
+                  <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={WaiterOrderNew} />
+                  <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={WaiterOrderId} />
 
-              </Switch>
-            </MainLayout>
-          </ThemeProvider>
-        </StylesProvider>
-      </BrowserRouter>
-    </div>
+                </Switch>
+              </MainLayout>
+            </ThemeProvider>
+          </StylesProvider>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
